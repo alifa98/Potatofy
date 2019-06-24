d415f1d0e5f3a9ac63bb5fdfcb42cbe8ed2f4d35
Ali Faraji <contact@alifaraji.ir>
2019-06-24 12:59:54 +0430

    CutomColors added

--- src/gui/CustomColors.java
+++ src/gui/CustomColors.java
@@ -1,4 +1,13 @@
 package gui;
 
+import java.awt.*;
+
 public class CustomColors {
+    public static final Color PRIMARY = new Color(94, 75, 149);
+    public static final Color PRIMARY_BRIGHT = new Color(188, 137,255);
+    public static final Color PRIMARY_BRIGHTER = new Color(235, 198,255);
+//    public static final Color PRIMARY = new Color(94, 75, 149);
+//    public static final Color PRIMARY = new Color(94, 75, 149);
+
+
 }
