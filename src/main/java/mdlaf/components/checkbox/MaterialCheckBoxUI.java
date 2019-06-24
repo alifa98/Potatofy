9a64ec138e6b00eb6cf59c793ccdeb430ff7eb4e
Erfan <er.hadizadeh@gmail.com>
2019-06-24 08:52:25 +0430

    material ui added

---
+++ src/main/java/mdlaf/components/checkbox/MaterialCheckBoxUI.java
@@ -0,0 +1,33 @@
+package mdlaf.components.checkbox;
+
+import mdlaf.utils.MaterialDrawingUtils;
+
+import javax.swing.*;
+import javax.swing.plaf.ComponentUI;
+import javax.swing.plaf.basic.BasicCheckBoxUI;
+import java.awt.*;
+
+public class MaterialCheckBoxUI extends BasicCheckBoxUI {
+
+	public static ComponentUI createUI (JComponent c) {
+		return new MaterialCheckBoxUI ();
+	}
+
+	@Override
+	public void installUI (JComponent c) {
+		super.installUI (c);
+
+		JCheckBox checkBox = (JCheckBox) c;
+		checkBox.setFont (UIManager.getFont ("CheckBox.font"));
+		checkBox.setBackground (UIManager.getColor ("CheckBox.background"));
+		checkBox.setForeground (UIManager.getColor ("CheckBox.foreground"));
+		checkBox.setIcon (UIManager.getIcon ("CheckBox.icon"));
+		checkBox.setSelectedIcon (UIManager.getIcon ("CheckBox.selectedIcon"));
+		checkBox.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+	}
+
+	@Override
+	public void paint (Graphics g, JComponent c) {
+		super.paint (MaterialDrawingUtils.getAliasedGraphics (g), c);
+	}
+}
