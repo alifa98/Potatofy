81f4b5c3abc70f4a6a1230bd2022be0456e4e136
Ali Faraji <contact@alifaraji.ir>
2019-06-24 08:55:59 +0430

    friend card added

--- src/gui/sidePanels/FriendCard.java
+++ src/gui/sidePanels/FriendCard.java
@@ -3,9 +3,9 @@ package gui.sidePanels;
 import com.TimeData;
 import com.UserInfoCard;
 import gui.ImageLabel;
+import gui.RoundedPanel;
 
 import javax.swing.*;
-import javax.xml.crypto.Data;
 import java.awt.*;
 import java.io.File;
 import java.util.Date;
@@ -15,6 +15,7 @@ public class FriendCard extends JPanel {
     public FriendCard(UserInfoCard userInfo) {
 
         //create and set grid bag layout to card
+        setBackground(Color.WHITE);
         GridBagLayout gridBagLayout = new GridBagLayout();
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
         this.setLayout(gridBagLayout);
@@ -25,8 +26,6 @@ public class FriendCard extends JPanel {
         gridBagConstraints.gridwidth = 2;
         gridBagConstraints.gridheight = 2;
         ImageLabel label = new ImageLabel(new ImageIcon(userInfo.getAvatarSrc().getPath()));
-        label.setOpaque(true);
-        label.setBackground(new Color(255,0,0));
         gridBagLayout.setConstraints(label, gridBagConstraints);
         this.add(label);
 
@@ -35,8 +34,6 @@ public class FriendCard extends JPanel {
         gridBagConstraints.gridwidth = 1;
         gridBagConstraints.gridheight = 1;
         JLabel friendNameLabel = new JLabel(userInfo.getFriendName());
-        friendNameLabel.setOpaque(true);
-        friendNameLabel.setBackground(new Color(255,255,0));
         gridBagLayout.setConstraints(friendNameLabel, gridBagConstraints);
         this.add(friendNameLabel);
 
@@ -45,9 +42,7 @@ public class FriendCard extends JPanel {
         gridBagConstraints.gridwidth = 1;
         gridBagConstraints.gridheight = 1;
         JLabel timeLabel = new JLabel(userInfo.getLastSeen().getRelativeTime());
-        timeLabel.setOpaque(true);
         gridBagLayout.setConstraints(timeLabel, gridBagConstraints);
-        timeLabel.setBackground(new Color(0,05,45));
         this.add(timeLabel);
 
         gridBagConstraints.gridx = 0;
@@ -55,8 +50,6 @@ public class FriendCard extends JPanel {
         gridBagConstraints.gridwidth = 3;
         gridBagConstraints.gridheight = 1;
         JLabel musicNameLabel = new JLabel(userInfo.getMusicName());
-        musicNameLabel.setOpaque(true);
-        musicNameLabel.setBackground(new Color(0,255,45));
         gridBagLayout.setConstraints(musicNameLabel, gridBagConstraints);
         this.add(musicNameLabel);
     }
@@ -64,12 +57,16 @@ public class FriendCard extends JPanel {
     public static void main(String[] args) {
         long d  = new Date().getTime() - 137777777;
         FriendCard c = new FriendCard(new UserInfoCard(
-                new File("D:\\works\\JAVA\\APProject\\src\\gui\\icons\\png\\64\\default-boy-avatar.png"),
+                new File("src\\gui\\icons\\png\\64\\default-boy-avatar.png"),
                 "Ali",
                 new TimeData(d), "Shape of my heart"));
         JFrame frame = new JFrame("Test");
+        JPanel p = new RoundedPanel(50,50,new Color(128,128,255),0,0,0);
+        p.add(c);
+        frame.setLayout(new FlowLayout());
+        frame.setSize(300,200);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.add(c);
+        frame.add(p);
         frame.setVisible(true);
     }
 }
